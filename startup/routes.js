ee57dff82e76a8d3726ab607f3166662bd6c1363
Thiago Barbosa da silva <thiagobarbosa@csc.gruporovema.com.br>
2019-08-27 16:13:12 -0400

    feat(routes): adiciona rotas para sessions

--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const users = require('../routes/users.route');
 const cigarettes = require('../routes/cigarettes.route');
+const sessions = require('../routes/session.route');
 
 module.exports = function(app) {
     app.use(express.json());
     app.use('/api/users', users);
     app.use('/api/cigarettes', cigarettes);
+    app.use('/api/sessions', sessions);
 }

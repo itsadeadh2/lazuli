d13db44d42c9f436bbfc467406a05c1f5624c6e7
Thiago Barbosa da silva <thiagobarbosa@csc.gruporovema.com.br>
2019-08-27 14:15:53 -0400

    misc

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const users = require('../routes/users.route');
+const cigarettes = require('../routes/cigarettes.route');
 
 module.exports = function(app) {
     app.use(express.json());
     app.use('/api/users', users);
+    app.use('/api/cigarettes', cigarettes);
 }
